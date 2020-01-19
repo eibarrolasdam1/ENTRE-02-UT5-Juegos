2c2053a16232f8fa0109061c9066b9a26cbca014
Eunate Ibarrola Santesteban (DAM 1) <eibarrolasdam1@gmail.com>
2020-01-19 13:03:25 +0100

    algo hay hecho

--- AppRevistaJuegosOnLine.java
+++ AppRevistaJuegosOnLine.java
@@ -5,11 +5,16 @@
  */
 public class AppRevistaJuegosOnLine 
 {
-	public static void main(String[] args)
-	{
-		
-
-		 
-	}
+    public static void main(String[] args)
+    {
+        if (args.length != 2) {
+            System.out.println("Error en argumentos" + "\n" + 
+                "Sintaxis: java AppRevistaJuegosOnLine <nombre> <n>"); 
+        }
+        int aux = Integer.parseInt(args[1]);
+        RevistaOnLineJuegos revista = new RevistaOnLineJuegos(args[0], aux);
+        revista.leerDeFichero();
+        revista.toString();
+    }
 
 }
