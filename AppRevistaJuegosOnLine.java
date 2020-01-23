1b1b3bb41e020dc56c24b0399aa1ddadc3a00b9a
Eunate Ibarrola Santesteban (DAM 1) <eibarrolasdam1@gmail.com>
2020-01-23 19:36:52 +0100

    Faltan algunas cosas

--- AppRevistaJuegosOnLine.java
+++ AppRevistaJuegosOnLine.java
@@ -15,6 +15,14 @@ public class AppRevistaJuegosOnLine
         RevistaOnLineJuegos revista = new RevistaOnLineJuegos(args[0], aux);
         revista.leerDeFichero();
         revista.toString();
+        revista.puntuar("Planet Zoo", 8);
+        revista.puntuar("Steep", 7);
+        revista.puntuar("Catastronauts", 9);
+        revista.puntuar("Wattam", 9);
+        revista.toString();
+        revista.valoracionMayorQue(8.2);
+        //revista.borrarDeGenero(ROL);
+        revista.toString();
     }
 
 }
--- RevistaOnLineJuegos.java
+++ RevistaOnLineJuegos.java
@@ -31,7 +31,7 @@ public class RevistaOnLineJuegos
      * Devuelve true si el array está completo, false en otro caso
      */
     public boolean estaCompleta() {
-         return total == juegos.length;
+        return total == juegos.length;
     }
 
     /**
@@ -46,36 +46,45 @@ public class RevistaOnLineJuegos
      *    
      */
     public void add(Juego juego) {
-         
+        if (estaCompleta()) {
+            System.out.println("El array esta completo, es decir, el juego no se puede añadir");
+        }
 
+        if (existeJuego(juego.getTitulo()) >= 0) {
+            System.out.println("El juego ya esta añadido, es decir no se puede duplicar");
+        }
+
+        if (!estaCompleta() && existeJuego(juego.getTitulo()) < 0) {
+
+        }
     }
-
-     
 
     /**
      * Efectúa una búsqueda en el array del juego cuyo titulo se
-     * recibe como parámetro. Es ndiferente mayúsculas y minúsculas
+     * recibe como parámetro. Es indiferente mayúsculas y minúsculas
      * Si existe el juego devuelve su posición, si no existe devuelve -1
      */
     public int existeJuego(String titulo) {
-         
-        
-        return 0;
-
+        int existe = -1;
+        for (int i = 0; i < total; i++) {
+            existe = juegos[i].getTitulo().compareToIgnoreCase(titulo);
+        }
+        return existe;
     }
 
     /**
-    * Representación textual de la revista
-    * Utiliza StringBuilder como clase de apoyo.
-    * Se incluye el nombre de la  revista on-line.
-    * (Ver resultados de ejecución)
-    */
+     * Representación textual de la revista
+     * Utiliza StringBuilder como clase de apoyo.
+     * Se incluye el nombre de la  revista on-line.
+     * (Ver resultados de ejecución)
+     */
     public String toString() {
-        
-        
-        
-        return "";
-
+        StringBuilder sb = new StringBuilder ("");
+        for (int i = 0; i < total; i++) {
+            sb.append(juegos[i].getTitulo()).append("\n").append("Genero: ").append(juegos[i].getGenero()).append(" | ").append("Lanzamiento: ").append(juegos[i].getYear()).append("\n").append("Valoración").append("(").append(juegos[i].getVotos()).append("votos").append("): ").append(juegos[i].getValoracionMedia());
+        }
+        String str = String.valueOf(sb);
+        return str;
     }
 
     /**
@@ -85,22 +94,42 @@ public class RevistaOnLineJuegos
      *  Si el juego no existe se muestra un mensaje en pantalla
      */
     public void puntuar(String titulo, int puntuacion) {
-         
-
+        for (int i = 0; i < total; i++) {
+            if (juegos[i].getTitulo().equalsIgnoreCase(titulo)) {
+                juegos[i].puntuar(puntuacion);
+            } else {
+                System.out.println("Eljuego que quieres puntuar no existe, por favor intentalo de nuevo");
+            }
+        }
     }
 
     /**
-    * Devuelve un array con los nombres de los juegos 
-    * con una valoración media mayor a la indicada  
-    * 
-    * El array se devuelve todo en mayúsculas y ordenado ascendentemente
-    */
+     * Devuelve un array con los nombres de los juegos 
+     * con una valoración media mayor a la indicada  
+     * 
+     * El array se devuelve todo en mayúsculas y ordenado ascendentemente
+     */
     public String[] valoracionMayorQue(double valoracion) {
-         
-        
-        
-        return null;
-
+        String aux = String.valueOf(valoracion);
+        int contador = 0;
+        for (int i = 0; i < total; i++) {
+            for (int j = 0;j < juegos[i].getValoraciones().length; j++) {
+                if (j > valoracion) {
+                    contador++;
+                }
+            }
+        }
+        String[] array = new String[contador];
+        int pos = 0;
+        for (int k = 0; k < array.length; k++) {
+            for (int j = 0;j < juegos[k].getValoraciones().length; j++) {
+                if (j > valoracion) {
+                    array[pos] = aux;
+                }
+            }
+            
+        }
+        return array;
     }
 
     /**
@@ -109,16 +138,14 @@ public class RevistaOnLineJuegos
      */
     public int borrarDeGenero(Genero genero) {
         
-        
         return 0;
-
     }
 
     /**
-    * Lee de un fichero de texto los datos de los juegos
-    * con ayuda de un objeto de la  clase Scanner
-    * y los guarda en el array. 
-    */
+     * Lee de un fichero de texto los datos de los juegos
+     * con ayuda de un objeto de la  clase Scanner
+     * y los guarda en el array. 
+     */
     public void leerDeFichero() {
         Scanner sc = null;
         try {
